ab030391b8f87854e430ba70abba7f0b5d306633
Jocelyn Gottschalk <jocelyn@Jocelyns-MacBook-Pro.local>
2017-06-18 20:49:20 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function myFunction(str) {
+	document.getElementById("demo").innerHTML=(str);
+}
+
+function myFunction2(str) {
+	document.getElementById("demo2").innerHTML=(str);
+}
+var str1="Happy";
+var str2="Father's Day!";
+
